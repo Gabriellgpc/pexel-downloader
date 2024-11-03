d7baa373e11dea8a5dc83889b69ad7b9d1814cd3
Luís Condados <condadoslgpc@gmail.com>
2024-11-02 22:41:00 -0400

    change version name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pexel_downloader',
-    version='0.3.1',
+    version='0.3.2',
     packages=find_packages(),
     install_requires=[
         "requests",
