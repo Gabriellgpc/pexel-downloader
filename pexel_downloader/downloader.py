eff219eb8bdffa031b22eaa017a3501136e3126b
Mimsseey <56138496+Mimsseey@users.noreply.github.com>
2025-11-04 23:30:25 +0100

    Update downloader.py

--- pexel_downloader/downloader.py
+++ pexel_downloader/downloader.py
@@ -3,7 +3,7 @@ import os
 import click
 from .pexel_downloader import PexelDownloader
 
-IMAGES_SIZES_LIST= ["original", "large2x" ,"large" ,"medium" ,"small" ,"portrait" ,"landscape", "tiny"]
+IMAGES_SIZES_LIST = ["original", "large2x", "large", "medium", "small", "portrait", "landscape", "tiny"]
 VIDEO_SIZES_LIST = ["large", "medium", "small"]
 
 @click.command()
@@ -12,31 +12,31 @@ VIDEO_SIZES_LIST = ["large", "medium", "small"]
 @click.argument("content-type", type=click.Choice(["image", "video"], case_sensitive=False))
 @click.option("--size", default="medium", help="Size of the image or video.")
 @click.option("--save-directory", "-o", default="downloads", help="Directory to save downloaded files.")
-def main(query, num, content_type, size, save_directory):
+@click.option("--start-page", "-p", default=1, type=int, help="Page number to start downloading from.")
+def main(query, num, content_type, size, save_directory, start_page):
     """
     CLI tool to download images or videos from Pexels.
-
     \b
     QUERY - The search term for the content.
     NUM - The number of images or videos to download.
     CONTENT_TYPE - The type of content to download (image or video).
-
     example of usage, downloading 5 videos of nature from Pexels:
     \b
-    \tpexel-downloader nature 5 videos
+    \tpexel-downloader nature 5 video --start-page 2
     """
     # Check for API key in environment variable if not provided as an option
     api_key = os.getenv("PEXEL_API_KEY")
     if not api_key:
         api_key = click.prompt("Please enter your Pexels API key", hide_input=True)
-
     # Initialize the downloader with the API key
     downloader = PexelDownloader(api_key=api_key)
-
     # Download images or videos based on the content type
+    # Passed start_page to both download_images and download_videos functions
     if content_type.lower() == "image":
-        downloader.download_images(query=query, num_images=num, save_directory=save_directory, size=size)
+        downloader.download_images(query=query, num_images=num, save_directory=save_directory, size=size, page=start_page)
     else:
-        downloader.download_videos(query=query, num_videos=num, save_directory=save_directory, size=size)
-
+        downloader.download_videos(query=query, num_videos=num, save_directory=save_directory, size=size, page=start_page)
     click.echo(f"[INFO] Downloaded {num} {content_type}s to {save_directory}")
+
+if __name__ == "__main__":
+    main()
